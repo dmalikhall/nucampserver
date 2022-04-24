0407762e032e5ca5754836aa6ff272a8503120a6
Derrion-Malik Hall <dmalikhall17@gmail.com>
2022-04-23 18:32:40 -0700

    week four workshop assignment

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '380411513967347',
+        clientSecret: '7dedac7b82f9dc1b2bb2ef4701812a61'
+    }
 }
